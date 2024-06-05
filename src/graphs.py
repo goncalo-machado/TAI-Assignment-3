e68f99527dbebcacdb7fb83e440822e02e21d8cf
Gonçalo Machado <goncalofmachado01@gmail.com>
2024-06-05 23:21:28 +0100

    accuracy base

--- src/graphs.py
+++ src/graphs.py
@@ -19,5 +19,25 @@ for result in base_results:
     else:
         correct_guess_dist[result["CorrectGuessPlace"]] += 1
 
-for key in correct_guess_dist.keys():
-    print(str(key) + " - " + str(correct_guess_dist[key]))
+top1 = 0
+top3 = 0
+top5 = 0
+top10 = 0
+total = 0
+
+for key, value in correct_guess_dist.items():
+    if key <= 10:
+        top10 += value
+    if key <= 5:
+        top5 += value
+    if key <= 3:
+        top3 += value
+    if key <= 1:
+        top1 += value
+    total += value
+
+print(top1/total)
+print(top3/total)
+print(top5/total)
+print(top10/total)
+print(total/total)
