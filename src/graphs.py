2e89fdc10086acbca4cb57b2742c3ed4cbe61314
Gonçalo Machado <goncalofmachado01@gmail.com>
2024-06-08 23:40:38 +0100

    graphs done

--- src/graphs.py
+++ src/graphs.py
@@ -1,147 +1,132 @@
+import csv
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-default_acc = {}
-noise_acc = {}
-compression_acc = {}
-duration_acc = {}
-
 with open('results/parsed_result.json') as f:
     base_results = json.load(f)
 
-# df = pd.DataFrame.from_records(base_results)
-
-# General Accuracy
-
-correct_guess_dist = {}
+res = {}
 
 for result in base_results:
-    if result["CorrectGuessPlace"] not in correct_guess_dist.keys():
-        correct_guess_dist[result["CorrectGuessPlace"]] = 0
+    n = str(result["Noise"])    
+    d = str(result["Duration"])
+    c = str(result["Compression"])
+    if result["CorrectGuessPlace"] != 1:
+        continue
+    if (n,d,c) not in res.keys():
+        res[(n,d,c)] = 1
     else:
-        correct_guess_dist[result["CorrectGuessPlace"]] += 1
+        res[(n,d,c)] += 1
 
-top1 = 0
-top3 = 0
-top5 = 0
-top10 = 0
-total = 0
+acc = []
 
-for key, value in correct_guess_dist.items():
-    if key <= 10:
-        top10 += value
-    if key <= 5:
-        top5 += value
-    if key <= 3:
-        top3 += value
-    if key <= 1:
-        top1 += value
-    total += value
+noise = ["0","0.02","0.04","0.08","0.16","0.32","0.64"]
+compression = ["zip", "gzip", "bzip2", "lzma", "zstd"]
+duration = ["10", "20", "40"]
 
-default_acc = {"Top1":top1,"Top3":top3,"Top5":top5,"Top10":top10}
+for n in noise:
+    for d in duration:
+        for c in compression:
+            acc.append({"Noise":n,"Compression":c,"Duration":d,"Top1":0})
 
-# Accuracy
+counter = 0
 
-total = 0
+for n in noise:
+    for d in duration:
+        for c in compression:
+            acc[counter]["Top1"] = res[(n,d,c)]
+            counter += 1
 
-for result in base_results:
-    noise = result["Noise"]
-    correct_guess_place = result["CorrectGuessPlace"]
-    if noise not in noise_acc.keys():
-        noise_acc[noise] = {"Top1":0, "Top3":0, "Top5":0, "Top10":0, "Total":0}
-    if correct_guess_place <= 1:
-        noise_acc[noise]["Top1"] += 1
-    if correct_guess_place <= 3:
-        noise_acc[noise]["Top3"] += 1
-    if correct_guess_place <= 5:
-        noise_acc[noise]["Top5"] += 1
-    if correct_guess_place <= 10:
-        noise_acc[noise]["Top10"] += 1
-    noise_acc[noise]["Total"] += 1
+counter = 0
+
+for n in noise:
+    for d in duration:
+        for c in compression:
+            acc[counter]["Top1"] = round(acc[counter]["Top1"] / 30, 3)
+            counter += 1
+
+keys = acc[0].keys()
+
+with open(".\\results\\accuracy.csv",'w', newline='') as csv_file:
+    dict_writer = csv.DictWriter(csv_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(acc)
+
+n_list = []
+d_list = []
+c_list = []
+
+for idx,n in enumerate(noise):
+    n_list.append([])
+for idx,d in enumerate(duration):
+    d_list.append([])
+for idx,c in enumerate(compression):
+    c_list.append([])
+
+for dic in acc:
+    for idx, n in enumerate(noise):
+        if dic["Noise"] == n:
+            n_list[idx].append(dic)
+    for idx,d in enumerate(duration):
+        if dic["Duration"] == d:
+            d_list[idx].append(dic)
+    for idx,c in enumerate(compression):
+        if dic["Compression"] == c:
+            c_list[idx].append(dic)
         
-for noise, item in noise_acc.items():
-    for top, value in noise_acc[noise].items():
-        noise_acc[noise][top] = value/noise_acc[noise]["Total"]
-print(noise_acc)
+n_pds = []
+d_pds = []
+c_pds = []
 
-# Accuracy By Compression
+for idx,n in enumerate(noise):
+    n_pds.append(pd.DataFrame.from_dict(n_list[idx]))
+for idx,d in enumerate(duration):
+    d_pds.append(pd.DataFrame.from_dict(d_list[idx]))
+for idx,c in enumerate(compression):
+    c_pds.append(pd.DataFrame.from_dict(c_list[idx]))
 
-total = 0
+plt.figure(1,figsize=(20,15))
 
-for result in base_results:
-    compression = result["Compression"]
-    correct_guess_place = result["CorrectGuessPlace"]
-    if compression not in compression_acc.keys():
-        compression_acc[compression] = {"Top1":0, "Top3":0, "Top5":0, "Top10":0, "Total":0}
-    if correct_guess_place <= 1:
-        compression_acc[compression]["Top1"] += 1
-    if correct_guess_place <= 3:
-        compression_acc[compression]["Top3"] += 1
-    if correct_guess_place <= 5:
-        compression_acc[compression]["Top5"] += 1
-    if correct_guess_place <= 10:
-        compression_acc[compression]["Top10"] += 1
-    compression_acc[compression]["Total"] += 1
-        
-for compression, item in compression_acc.items():
-    for top, value in compression_acc[compression].items():
-        compression_acc[compression][top] = value/compression_acc[compression]["Total"]
-# print(compression_acc)
+for idx,n in enumerate(noise):
+    subplot = 420 + idx + 1
+    plt.subplot(subplot)
+    p = sns.lineplot(x='Compression', y='Top1', hue='Duration',data=n_pds[idx],linestyle='-', marker='o', markersize=10)
+    plt.xlabel('Compression')
+    plt.ylabel('Accuracy (%)')
+    plt.title('Noise ' + n)
+    plt.ylim((0.5, 1.1))
 
-# Accuracy By Duration
+plt.subplots_adjust(wspace=0.2,hspace=0.46, right=0.72,top=0.94)
 
-total = 0
+plt.savefig('./results/noise.png')
 
-for result in base_results:
-    duration = result["Duration"]
-    correct_guess_place = result["CorrectGuessPlace"]
-    if duration not in duration_acc.keys():
-        duration_acc[duration] = {"Top1":0, "Top3":0, "Top5":0, "Top10":0, "Total":0}
-    if correct_guess_place <= 1:
-        duration_acc[duration]["Top1"] += 1
-    if correct_guess_place <= 3:
-        duration_acc[duration]["Top3"] += 1
-    if correct_guess_place <= 5:
-        duration_acc[duration]["Top5"] += 1
-    if correct_guess_place <= 10:
-        duration_acc[duration]["Top10"] += 1
-    duration_acc[duration]["Total"] += 1
-        
-for duration, item in duration_acc.items():
-    for top, value in duration_acc[duration].items():
-        duration_acc[duration][top] = value/duration_acc[duration]["Total"]
-# print(duration_acc)
+plt.figure(2,figsize=(20,4))
 
-#Graphs
+for idx,d in enumerate(duration):
+    subplot = 130 + idx + 1
+    plt.subplot(subplot)
+    p = sns.lineplot(x='Noise', y='Top1', hue='Compression',data=d_pds[idx],linestyle='-', marker='o', markersize=10)
+    plt.xlabel('Noise')
+    plt.ylabel('Accuracy (%)')
+    plt.title('Duration ' + d)
+    plt.ylim((0.5, 1.1))
 
-noise_acc_list = []
-compression_acc_list = []
-duration_acc_list = []
+plt.savefig('./results/duration.png')
 
-for key, value in noise_acc.items():
-    temp_dic = value
-    temp_dic["Noise"] = key
-    noise_acc_list.append(temp_dic)
+plt.figure(3,figsize=(20,15))
 
-# print(noise_acc_list)
+for idx,c in enumerate(compression):
+    subplot = 320 + idx + 1
+    plt.subplot(subplot)
+    p = sns.lineplot(x='Noise', y='Top1', hue='Duration',data=c_pds[idx],linestyle='-', marker='o', markersize=10)
+    plt.xlabel('Noise')
+    plt.ylabel('Accuracy (%)')
+    plt.title('Compression ' + c)
+    plt.ylim((0.5, 1.1))
 
-noise_pd = pd.DataFrame.from_dict(noise_acc_list)
-# compression_pd = pd.DataFrame.from_dict(compression_acc)
-# duration_pd = pd.DataFrame.from_dict(duration_acc)
+plt.subplots_adjust(hspace=0.34)
 
-# print(noise_pd.head())
-
-noise_pd = pd.melt(noise_pd, id_vars=['Noise'],value_vars=['Top1','Top3','Top5','Top10'])
-
-# print(noise_pd.head(10))
-
-plt.figure(1,figsize=(12,6))
-p1 = sns.lineplot(x='variable', y='value',hue='Noise',data=noise_pd,linestyle='-', marker='o', markersize=10)
-plt.xlabel('Dataset')
-plt.ylabel('Accuracy (%)')
-plt.title('Alpha = 1')
-
-plt.show()
+plt.savefig('./results/compression.png')
